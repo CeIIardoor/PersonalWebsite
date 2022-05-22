15a83e0ba848950ba0e82a851638f30b32fe919f
Cellardoor <cellardoorweb@gmail.com>
2022-05-22 22:38:42 +0000

    update meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="google-site-verification" content="gFVUQ1BTkoc_2EyEXXS8M_j3IhlYKD4empPt0ISLOy4" />
+    <meta name="description" content="Yassine Laouina, Ingénieur Informatique et Réseaux"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
     <script src="{{ mix('/js/manifest.js') }}" defer></script>
